3a18ccad36cf0ffb57109f605b8e24d5c92cd825
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-07-26 20:02:34 +0900

    ⚡ Update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,46 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::sync::Mutex;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn health() -> bool {
+    toy_ufdb::Ufdb::new().is_empty()
+}
+
+#[tauri::command]
+fn make_set(key: String, state: tauri::State<Mutex<toy_ufdb::Ufdb>>) -> bool {
+    let mut ufdb = state.lock().unwrap();
+    ufdb.make_set(&key)
+}
+
+#[tauri::command]
+fn groups(state: tauri::State<Mutex<toy_ufdb::Ufdb>>) -> Vec<Vec<String>> {
+    let mut ufdb = state.lock().unwrap();
+
+    let mut groups: Vec<Vec<String>> = ufdb
+        .groups()
+        .into_values()
+        .map(|group| group.into_iter().cloned().collect())
+        .collect();
+
+    for group in &mut groups {
+        group.sort();
+    }
+
+    groups.sort_by(|a, b| b.len().cmp(&a.len()));
+
+    groups
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(Mutex::new(toy_ufdb::Ufdb::new()))
+        // .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, health, make_set, groups])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,55 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [key, setKey] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+  async function hello() {
+    const res = await invoke("health");
+
+    console.log("res = ", res);
   }
+
+  async function insert() {
+    const res = await invoke("make_set", { key });
+
+    console.log("res = ", res);
+
+    setGroups(await invoke<string[][]>("groups"));
+  }
+
+  const [groups, setGroups] = useState<string[][]>([]);
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>UFDB GUI APP</h1>
 
       <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
         <a href="https://tauri.app" target="_blank">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          insert();
         }}
       >
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          value={key}
+          onChange={(e) => setKey(e.currentTarget.value)}
+          placeholder="Enter a key..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Insert</button>
       </form>
-      <p>{greetMsg}</p>
+
+      <h1>森</h1>
+      <ul>
+        {groups.map((group, i) => (
+          <li key={i}>{group.join(", ")}</li>
+        ))}
+      </ul>
     </main>
   );
 }
